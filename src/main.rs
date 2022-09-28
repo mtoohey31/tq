0106ecef02a135257f32d0bcd152e80c1300277a
Matthew Toohey <contact@mtoohey.com>
2022-09-27 21:15:19 -0400

    feat: better error handling and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,48 @@
+use anyhow::anyhow;
+use either::Either;
 use json::JsonValue;
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::env;
-use std::error::Error;
-use std::ffi::OsString;
-use std::fs::read_to_string;
-use std::fs::File;
-use std::path::Path;
-use std::process::Command;
-use tempdir::TempDir;
+use std::{
+    env,
+    error::Error,
+    ffi::{OsStr, OsString},
+    fs::read_to_string,
+    path::Path,
+    process::Command,
+};
+use tempfile::TempPath;
 use toml::Value as TomlValue;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let tmp_dir = TempDir::new("tq")?;
-    let tmp_dir_path = tmp_dir.path();
+    let transformed = env::args_os()
+        .skip(1)
+        .map(transform_argument)
+        .collect::<Result<Vec<_>, anyhow::Error>>()?;
     Command::new("jq")
-        .args(
-            env::args_os()
-                .skip(1)
-                .map(|arg| transform_argument(arg, tmp_dir_path))
-                .collect::<Vec<OsString>>(),
-        )
+        .args(transformed.iter().map(|e| match e {
+            Either::Left(os) => os.as_os_str(),
+            Either::Right(tp) => tp.as_os_str(),
+        }))
         .status()
-        .expect("jq failed to start");
-    tmp_dir.close()?;
+        .map_err(|err| anyhow!("jq failed to start: {}", err))?;
     Ok(())
 }
 
-fn transform_argument(arg: OsString, tmp_dir_path: &Path) -> OsString {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"^.*\.toml$").unwrap();
+fn transform_argument(arg: OsString) -> Result<Either<OsString, TempPath>, anyhow::Error> {
+    if Path::new(&arg).extension() != Some(OsStr::new("toml")) {
+        return Ok(Either::Left(arg));
     }
-    let lossy_arg = arg.clone();
-    let lossy_arg = lossy_arg.to_string_lossy();
-    if RE.is_match(&lossy_arg) {
-        let toml_value: TomlValue = toml::from_str(
-            &read_to_string(&arg)
-                .unwrap_or_else(|err| panic!("error reading {}: {}", lossy_arg, err)),
-        )
-        .unwrap_or_else(|err| panic!("error parsing {}: {}", lossy_arg, err));
-        let tmp_file_path = tmp_dir_path.join(arg);
-        to_json(toml_value)
-            .write(
-                &mut File::create(&tmp_file_path)
-                    .unwrap_or_else(|err| panic!("error opening temp file: {}", err)),
-            )
-            .unwrap_or_else(|err| panic!("error writing to temp file: {}", err));
-        tmp_file_path
-            .canonicalize()
-            .unwrap()
-            .as_os_str()
-            .to_os_string()
-    } else {
-        arg
-    }
+
+    let toml_value: TomlValue = toml::from_str(
+        &read_to_string(&arg)
+            .map_err(|err| anyhow!("error reading {}: {}", arg.to_string_lossy(), err))?,
+    )
+    .map_err(|err| anyhow!("error parsing {}: {}", arg.to_string_lossy(), err))?;
+    let named_tmp_file = tempfile::NamedTempFile::new()
+        .map_err(|err| anyhow!("error creating tempfile: {}", err))?;
+    to_json(toml_value)
+        .write(&mut named_tmp_file.as_file())
+        .map_err(|err| anyhow!("error writing to temp file: {}", err))?;
+    Ok(Either::Right(named_tmp_file.into_temp_path()))
 }
 
 fn to_json(value: TomlValue) -> JsonValue {
