93c731ec4d9c11e83fe5272685a489d6d78b094b
Matthew Toohey <contact@mtoohey.com>
2022-09-27 21:15:23 -0400

    build: add flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "tq";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }: {
+    overlays = rec {
+      expects-naersk = final: prev: {
+        tq = final.naersk.buildPackage {
+          pname = "tq";
+          root = ./.;
+        };
+      };
+
+      default = final: prev: {
+        inherit (prev.appendOverlays [
+          naersk.overlay
+          expects-naersk
+        ]) tq;
+      };
+    };
+  } // utils.lib.eachDefaultSystem (system: with import nixpkgs
+    { overlays = [ self.overlays.default ]; inherit system; }; rec {
+    packages.default = tq;
+
+    devShells.default = mkShell {
+      packages = [
+        cargo
+        cargo-watch
+        (writeShellScriptBin "jq" ''
+          echo "$@"
+          ${jq}/bin/jq "$@"
+        '')
+        rust-analyzer
+        rustc
+        rustfmt
+      ];
+      shellHook = ''
+        export RUST_SRC_PATH=${rustPlatform.rustLibSrc}
+      '';
+    };
+  });
+}
